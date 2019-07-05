d1605dd83b4e33c177796918b32f854bf2241fa7
Jaxcoder <codenamejason@outlook.com>
2019-07-05 13:58:37 -0400

    Module 4. Quiz end bool and page.

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import QuizQuestion from './QuizQuestion.js';
+import QuizEnd from './QuizEnd.js';
+
 
 let quizData = require('./quiz_data.json');
 
@@ -13,11 +15,15 @@ class Quiz extends Component {
         }
     }
 
+    
+
     render() {
+        const isQuizEnd = ((this.state.quiz_position - 1) === (quizData.quiz_questions.length));
         return (
             <div>
+                {isQuizEnd ? <QuizEnd /> :
                 <QuizQuestion quiz_question={quizData.quiz_questions
-                    [this.state.quiz_position - 1]}/>
+                    [this.state.quiz_position - 1]}/>}
             </div>
         );
     }
