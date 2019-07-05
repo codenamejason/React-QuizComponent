44a3aa999d458e019d83018e870891a7a7b6a2c5
Jaxcoder <codenamejason@outlook.com>
2019-07-05 14:35:54 -0400

    Module 5

--- src/App.js
+++ src/App.js
@@ -5,7 +5,21 @@ import Quiz from './Quiz.js';
 class App extends Component {
   render() {
     return (
-      <Quiz />
+      <div>
+        <Quiz />
+        <hr />
+        <Footer />
+      </div>
+    )
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <p className="text-center">2019 Jaxcoder</p>
+      </div>
     )
   }
 }
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,18 +2,29 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            incorrectAnswer: false,
+        }
+    }
+    
     handleClick(buttonText){
-        console.group('QuizQuestion Component')
+        console.group('QuizQuestion handleClick event')
         console.log(`Quiz Answer:`, buttonText)
 
         if(buttonText === this.props.quiz_question.answer){
+            console.log('Correct!')
+            this.setState({incorrectAnswer: false})
             this.props.showNextQuestionHandler();
+        } else {
+            this.setState({incorrectAnswer: true})
+            console.log('Incorrect!!!!')
         }
+        
 
-
-    }
-
-    
+    }    
 
     render() {
         return (
@@ -30,6 +41,8 @@ class QuizQuestion extends Component {
                         
                     </ul>
                 </section>
+                { this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : 
+                null }
             </main>
         )
     }
